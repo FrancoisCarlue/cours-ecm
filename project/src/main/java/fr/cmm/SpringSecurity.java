08ced05a4acbbe8076afc29bda5900665c90f1bb
Francois Carlue <francois.carlue@centrale-marseille.fr>
2018-10-12 13:46:05 +0200

    MVC10 - put password to admin session

---
+++ project/src/main/java/fr/cmm/SpringSecurity.java
@@ -0,0 +1,38 @@
+package fr.cmm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Francois on 12/10/2018.
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
+    @Inject
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/**").authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+}
